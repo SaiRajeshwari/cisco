14370c1b1d724cbd40c120fd51c746e826211467
Sai Rajeshwari <sairajeshwari.g@gmail.com>
2020-04-22 23:02:42 -0700

    Get basic build data

--- helloworld_project/pages/views.py
+++ helloworld_project/pages/views.py
@@ -16,5 +16,8 @@ def homePageView(request):
         res.append(str(hit.meta.id))
         res.append(str(hit.data.id))
         res.append(str(hit.data.projectName))
-    return render_to_response('pages/base.html', {'jenkins_data': ' : '.join(res)})
+        res.append(str(hit.data.result))
+        l = [item for item in hit.message if len(item)>1 and item[0]=='+']
+        res.append(l)
+    return render_to_response('pages/base.html', {'jenkins_data': res})
 
